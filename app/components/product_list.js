d89f9862edf3df394c27975311fef8cbfe585ba7
budi7 <budi-purnomo@outlook.com>
2018-06-07 14:47:20 +0700

    update product list

--- app/components/product_list.js
+++ app/components/product_list.js
@@ -1,7 +1,7 @@
 module.exports = {
     methods: {
         onItemTap: function (args) {
-            this.$emit('productlistTapped', args);
+            this.$emit('productlistTapped', this.products[args.index] );
         }
     },
 
@@ -9,35 +9,37 @@ module.exports = {
         return {
             products: [
                 {
-                    name: "Echo Dot (2nd Generation)",
+                    id: 1,
+                    name: "Echo Dot (2nd Generation) kodot kodot kodot kodot dot",
                     imageSrc: "https://images-na.ssl-images-amazon.com/images/I/61ikAJnULvL._SY450_.jpg",
-                    price: "IDR 1.480.000",
-                    sku: "SKU : 10367170",
-                    stock: "Stock : 23",
+                    price: "IDR 1.500.000",
+                    stock: "Stock : 12",
+                    sku: "SKU : 90"
                 },
                 {
-                    name: "Wi-Fi Smart Plug, Mini Outlets Smart Socket No Hub Required Timing Function Control Your Electric Devices from Anywhere Works with Amazon Alexa and Google Assistant 4-Pack Amysen",
-                    imageSrc: "https://images-na.ssl-images-amazon.com/images/I/61NNHutN9AL._SL1200_.jpg",
-                    price: "IDR 1.780.000",
-                    sku: "SKU : 10367171",
-                    stock: "Stock : 21",
-                },
+                    id: 2,
+                    name: "Echo Dot (2nd Generation) kodot kodot kodot kodot dot",
+                    imageSrc: "https://images-na.ssl-images-amazon.com/images/I/61ikAJnULvL._SY450_.jpg",
+                    price: "IDR 1.500.000",
+                    stock: "Stock : 12",
+                    sku: "SKU : 90"
+                },                
             ],
         };
     },
     template: `
-    <ListView class="list-group" for="product in products" @itemTap="onItemTap" style="height:1250px">
+    <ListView class="list-group" for="product in products" @itemTap="onItemTap" style="height:10000;">
         <v-template>
-
-            <FlexboxLayout flexDirection="row">
-                <GridLayout columns="auto, auto, *" rows="auto, auto, *" class="list-group-item">
-                    <Image :src="product.imageSrc" class="thumb" row="0" col="0" rowSpan="2"/>
-                    <label :text="product.sku" class="h6" row="0" col="1"/>
-                    <label :text="product.name" width="100%" textWrap="true" class="h4" row="1" col="1"/>
-                    <Label :text="product.price" class="h5" row="2" col="1"/>
-                </GridLayout>        
-            </FlexboxLayout>      
-
+            <StackLayout>
+                <GridLayout columns="auto, *, auto" rows="auto, auto, auto" margin="12, 12, 6, 12" >
+                    <Image :src="product.imageSrc" rowSpan="3" width="54" height="54" verticalAlignment="top" margin="0,12,0,0"/>
+                    <Label :text="product.name" row="0" col="1" colSpan="2" class="list-group-item-heading" verticalAlignment="top" textWrap="false" margin="0"/>
+                    <Label :text="product.sku" row=1 col="1" colspan="2" class="h6" margin="0"/>
+                    <Label :text="product.price" row="2" col="1" class="h6" horizontalAlignment="left" />
+                    <Label text="Stok Ada" row="2" col="2" class="h5 text-success" verticalAlignment="bottom" />
+                    <Label text="" col="3" row="3" colspan="2" class="h6"/>
+                </GridLayout>
+            </StackLayout>
         </v-template>
     </ListView>
     `
