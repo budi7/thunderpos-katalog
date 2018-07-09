86fa1655fe4ef7162445418b144ee67f0d389b51
budi7 <budi-purnomo@outlook.com>
2018-07-09 14:40:10 +0700

    product list display

--- app/components/product_list.js
+++ app/components/product_list.js
@@ -1,29 +1,67 @@
 module.exports = {
-    methods: {
-        onItemTap: function (args) {
-            this.$emit('productlistTapped', this.products[args.index] );
-        }
-    },
     props: {
         'products': []
     },
     data() {
-        return {};
+        return {
+            ui_helper : {
+                text_status : null,
+                text_color : null,
+            }
+        };
     },
     template: `
     <ListView class="list-group" for="product in products" @itemTap="onItemTap" style="height:10000;">
         <v-template>
             <StackLayout>
+                {{ productStatus(product) }}
                 <GridLayout columns="auto, *, auto" rows="auto, auto, auto" margin="12, 12, 6, 12" >
                     <Image :src="product.imageSrc" rowSpan="3" width="54" height="54" verticalAlignment="top" margin="0,12,0,0"/>
                     <Label :text="product.name" row="0" col="1" colSpan="2" class="list-group-item-heading" verticalAlignment="top" textWrap="false" margin="0"/>
-                    <Label :text="product.sku" row=1 col="1" colspan="2" class="h6" margin="0"/>
+                    <Label :text="'SKU : ' + (product.config.is_stock ? product.sku : '_')" row=1 col="1" colspan="2" class="h6" margin="0"/>
                     <Label :text="product.price" row="2" col="1" class="h6" horizontalAlignment="left" />
-                    <Label :text="'Stok : ' + product.stock" row="2" col="2" class="h5 text-success" verticalAlignment="bottom" />
+                    <Label :text="ui_helper.text_status" row="2" col="2" :class="'h5 ' + ui_helper.text_color" verticalAlignment="bottom" />
                     <Label text="" col="3" row="3" colspan="2" class="h6"/>
                 </GridLayout>
             </StackLayout>
         </v-template>
     </ListView>
-    `
+    `,
+    methods: {
+        onItemTap: function (args) {
+            this.$emit('productlistTapped', this.products[args.index] )
+        },
+        productStatus: function(product){
+            var config = product.config
+
+            if(config.is_sellable){
+                if(config.is_stock){
+                    if(product.stock == 0){
+                        this.ui_helper.text_status = "Stok Habis"
+                        this.ui_helper.text_color = "text-danger"
+                    }else{
+                        if(config.is_stock_warning){
+                            if(product.stock <= config.stock_limit){
+                                this.ui_helper.text_status = "Stok Kritis"
+                                this.ui_helper.text_color = "text-warning"
+                            }else{
+                                this.ui_helper.text_status = "Stok Ada"
+                                this.ui_helper.text_color = "text-success"
+                            }
+                        }else{
+                            this.ui_helper.text_status = "Stok Ada"
+                            this.ui_helper.text_color = "text-success"
+                        }
+                    }
+                }else{
+                    this.ui_helper.text_status = "Tersedia"
+                    this.ui_helper.text_color = "text-success"
+                }
+            }else{
+                this.ui_helper.text_status = "Tidak Tersedia"
+                this.ui_helper.text_color = "text-danger"
+            }
+        },
+
+    }
 };
---
+++ app/dummies/products.js
@@ -0,0 +1,74 @@
+module.exports = {
+    data : [
+        {
+            id: 1,
+            name: "Tolak Angin Cair",
+            imageSrc: "https://s0.bukalapak.com/img/5703897242/w-1000/Tolak_Angin_Cair.jpg",
+            price: "IDR 2.500",
+            stock: "240",
+            sku: "ob-tlkagn-cr",
+            config: {
+                is_stock: true,
+                is_sellable: true,
+                is_stock_warning : true,
+                stock_limit: 12
+            }
+        },
+        {
+            id: 2,
+            name: "Tolak Angin Anak Cair",
+            imageSrc: "https://s0.bukalapak.com/img/5703897242/w-1000/Tolak_Angin_Cair.jpg",
+            price: "IDR 3.500",
+            stock: "2",
+            sku: "ob-tlkagn-crank",
+            config: {
+                is_stock: true,
+                is_sellable: true,
+                is_stock_warning : true,
+                stock_limit: 12
+            }
+        }, 
+        {
+            id: 3,
+            name: "Tolak Angin Tablet",
+            imageSrc: "https://s0.bukalapak.com/img/5703897242/w-1000/Tolak_Angin_Cair.jpg",
+            price: "IDR 5.000",
+            stock: "0",
+            sku: "ob-tlkagn-tbl",
+            config: {
+                is_stock: true,
+                is_sellable: true,
+                is_stock_warning : true,
+                stock_limit: 12
+            }
+        }, 
+        {
+            id: 3,
+            name: "Tolak Angin Tablet",
+            imageSrc: "https://s0.bukalapak.com/img/5703897242/w-1000/Tolak_Angin_Cair.jpg",
+            price: "IDR 5.000",
+            stock: "0",
+            sku: "ob-tlkagn-tbl",
+            config: {
+                is_stock: true,
+                is_sellable: false,
+                is_stock_warning : true,
+                stock_limit: 12
+            }
+        }, 
+        {
+            id: 4,
+            name: "Tensi Darah",
+            imageSrc: "https://s0.bukalapak.com/img/5703897242/w-1000/Tolak_Angin_Cair.jpg",
+            price: "IDR 5.000",
+            stock: "0",
+            sku: "js-tns",
+            config: {
+                is_stock: false,
+                is_sellable: true,
+                is_stock_warning : false,
+                stock_limit: null
+            }
+        },                       
+    ]
+}
